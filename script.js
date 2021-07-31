173046915c1634bb3b2a2725e3be7842715c7df8
Ninua-Jin <ni.jintcharadze@gmail.com>
2021-07-31 22:30:27 +0400

    ajax

--- script.js
+++ script.js
@@ -4,3 +4,67 @@ let burger = document.getElementById('bar');
 burger.addEventListener('click', () => {
     navbar.classList.toggle('active');
 })
+
+
+// ajax
+
+
+function getEmploy() {
+
+    function render() {
+        let response = this.responseText;
+        let final_Data = JSON.parse(response);
+        console.log(final_Data);
+
+        let ul = document.createElement('ul');
+
+        final_Data.data.forEach(element => {
+            let li = document.createElement('li');
+            li.textContent = element.name;
+
+            ul.appendChild(li);
+        });
+        // let li = document.createElement('li');
+        // li.textContent = finData.data[3].name;
+
+        // ul.appendChild(li);
+
+        document.getElementById('ajax-div').appendChild(ul);
+    }
+
+    let req = new XMLHttpRequest();
+
+    req.addEventListener('load', render);
+
+    req.open('GET', 'https://reqres.in/api/unknown');
+    req.send();
+}
+
+getEmploy();
+
+// function getEmploy() {
+
+//     function render() {
+//         let response = this.responseText;
+//         let data_ = JSON.parse(response);
+//         console.log(data_);
+
+//         let ul = document.createElement('ul');
+//         let li = document.createElement('li');
+//         li.textContent = data_.data[2].email;
+
+//         ul.appendChild(li);
+   
+
+//         document.getElementById('ajax-div').appendChild(ul);
+//     }
+//     let req = new XMLHttpRequest();
+
+//     req.addEventListener('load', render);
+
+//     req.open('GET', 'https://reqres.in/api/users?page=2');
+//     req.send();
+// }
+
+// getEmploy();
+
